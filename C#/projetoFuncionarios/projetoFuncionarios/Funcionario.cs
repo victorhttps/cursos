490bf37d502aba0ecd82de245983223826aca9ae
Victor Hugo <76017410+victorhttps@users.noreply.github.com>
2021-04-09 11:54:54 -0300

    Commit de projeto

---
+++ C#/projetoFuncionarios/projetoFuncionarios/Funcionario.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace projetoFuncionarios
+{
+    class Funcionario
+    {
+        public String Nome { get; set; }
+        public int Id { get; set; }
+        public double Salario { get; set; }
+
+        
+        public Funcionario(string nome, int id, double salario)
+        {
+            Nome = nome;
+            Id = id;
+            Salario = salario;
+        }
+
+        public void Aumentar( double porcentagem)
+        {
+             Salario += Salario * (porcentagem/100);
+        }
+
+        public override string ToString()
+        {
+            return "Id: " + Id + 
+                   "\nNome: " + Nome +
+                   "\nSalario: " + Salario.ToString("F2") +
+                   "\n";
+        }
+    }
+}
---
+++ C#/projetoFuncionarios/projetoFuncionarios/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace projetoFuncionarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Quantos funcionarios serão registrados? : ");
+            int qtd = int.Parse(Console.ReadLine());
+
+            List<Funcionario> list = new List<Funcionario>();
+
+            for (int i = 1; i <= qtd; i++)
+            {
+                Console.WriteLine("Funcionario #" + i);
+                Console.Write("Id: ");
+                int Id = int.Parse(Console.ReadLine());
+                Console.Write("Nome: ");
+                string Nome = Console.ReadLine();
+                Console.Write("Salario: ");
+                double Salario = double.Parse(Console.ReadLine());
+                list.Add(new Funcionario(Nome, Id, Salario));
+                Console.WriteLine("");
+            }
+
+            Console.Write("Digite o ID do funcionario que tera o salario aumentado: ");
+            int IdSrc = int.Parse(Console.ReadLine());
+
+            Funcionario emp = list.Find(x=> x.Id == IdSrc);
+            if(emp != null)
+            {
+                Console.Write("Escreva a porcentagem: ");
+                double Porcentagem = double.Parse(Console.ReadLine());
+                emp.Aumentar(Porcentagem);
+            }
+            else
+            {
+                Console.WriteLine("Esse Id Não existe");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Lista atualizada de funcionarios: ");
+            foreach(Funcionario funcionario in list)
+            {
+                Console.WriteLine(funcionario);
+            }
+        }
+    }
+}
